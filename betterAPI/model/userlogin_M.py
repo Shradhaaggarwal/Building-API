abddf91b9f430e47aa88d8d571cf71d2fc8bd0ce
Shradhaaggarwal <shradhaaggarwal1402@gmail.com>
2025-10-31 16:34:09 +0530

    -connecting db and rendering data on brower

--- betterAPI/model/userlogin_M.py
+++ betterAPI/model/userlogin_M.py
@@ -1,4 +1,24 @@
+#model mai muje 2 code likhne pdege, ik connection ke liye aur ik operations ke liye, isliye connection wala toh constructor m dal dege
+import json
+import mysql.connector
 
-class userlogin_M:
+class userlogin_M:  
+    def __init__(self):
+        try:
+            self.con = mysql.connector.connect(host="localhost", user="Shradha", password="Shradha1402@", database="learningapi")
+            self.curr = self.con.cursor(dictionary=True)
+            print("Connection Successful")
+        except mysql.connector.Error as e:
+            print("Connection Failed", e)
+
     def userlogin_model(self):
-        return "User Login Model Page"
+        try: 
+            self.curr.execute("SELECT * FROM useracc")
+            result = self.curr.fetchall()
+            if len(result) > 0:
+                return result
+            else:
+                return "No Data Found"
+        # return "User Login Model Page"
+        except mysql.connector.Error as e:
+            return f"Error fetching data: {e}"
